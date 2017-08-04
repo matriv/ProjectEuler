5e50675207afc8bfd7695c2ceb53a81b7c5eb924
Marios Trivyzas <marios@crate.io>
2017-08-04 12:30:16 +0300

    fixup: lala

--- src/org/dev/matriv/Euler5.java
+++ src/org/dev/matriv/Euler5.java
@@ -1,15 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.dev.matriv;
 
 /**
  * @author matriv
  */
 public class Euler5 {
-
 
     public static void main(String[] args) {
         int a = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19;
